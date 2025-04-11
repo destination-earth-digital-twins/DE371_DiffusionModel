bug fix on the way to retrieve and broadcast the ensemble mean over the batch/conditions

ddpm/dataSet_Handler.py
@@ -134,20 +134,27 @@ class ISDataset(Dataset):
             if var_cond:
                 var = mean_var_file[1].unsqueeze(0).expand(self.n_conditioning_sets, -1, -1, -1)
                 condition_tensor = torch.cat([condition_tensor, var], dim=1)
-        else:
-            normalized_ensemble_mean_tensor = torch.zeros(self.n_conditioning_sets, self.config.n_var*self.config.n_conditions, self.height_dim, self.width_dim) # 0 tensor -> member = member + 0 when sampling
 
 
         sample_id = re.search(r"\d+", file_name).group()
 <<<<<<< HEAD
+<<<<<<< HEAD
         return {"id_in_csv": idx, "img": sample, "img_id": sample_id, "condition_tensor": condition_tensor, "ensemble_mean_tensor": ensemble_mean_tensor, "member_id": member, "date": date, "leadtime": lt}
 =======
+=======
+>>>>>>> c34caea (bug fix on the way to retrieve and broadcast the)
 <<<<<<< HEAD
         return {"id_in_csv": idx, "img": sample, "img_denorm":sample_denorm, "img_id": sample_id, "condition_tensor": condition_tensor, "condition_tensor_denorm" : condition_tensor_denorm,"member_id": member, "date": date, "leadtime": lt}
 =======
         return {"id_in_csv": idx, "img": sample, "img_id": sample_id, "condition_tensor": condition_tensor, "ensemble mean tensor": normalized_ensemble_mean_tensor, "member_id": member, "date": date, "leadtime": lt}
 >>>>>>> c9f46e6 (Enables training wit the "predict_residue" option)
+<<<<<<< HEAD
 >>>>>>> 0489d78 (Enables training wit the "predict_residue" option)
+=======
+=======
+        return {"id_in_csv": idx, "img": sample, "img_id": sample_id, "condition_tensor": condition_tensor, "ensemble mean tensor": ensemble_mean_tensor, "member_id": member, "date": date, "leadtime": lt}
+>>>>>>> e7ab104 (bug fix on the way to retrieve and broadcast the)
+>>>>>>> c34caea (bug fix on the way to retrieve and broadcast the)
 
     def get_conditioning_members(self, ensemble_df, idx,return_denorm=False):
         """