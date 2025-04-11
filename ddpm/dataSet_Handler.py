c34caea53dcf36bf9674ccd65c789c2b1541a484
Maxence Lame <maxence.lame@meteo.fr>
2025-04-11 14:29:30 +0200

    bug fix on the way to retrieve and broadcast the ensemble mean over the batch/conditions

--- ddpm/dataSet_Handler.py
+++ ddpm/dataSet_Handler.py
@@ -111,12 +111,13 @@ class ISDataset(Dataset):
         row = ensemble_df.iloc[0] if not ensemble_df.empty else {"Date": "", "LeadTime": 0, "Member": ""}
         date = str(pd.to_datetime(row["Date"]).strftime('%Y-%m-%d'))
         lt = row["LeadTime"]
-        
         if self.config.guiding_col is not None:
             member = row[self.config.guiding_col]
         else:
             member = row["Member"]
-         # Optional configs based on the ensemble mean and variance
+
+        # Optional configs based on the ensemble mean and variance
+        ensemble_mean_tensor = torch.zeros(self.n_conditioning_sets, self.config.n_var*self.config.n_conditions, self.height_dim, self.width_dim) # 0 tensor -> member = member + 0 when sampling
         if mean_cond or var_cond or self.config.predict_residue:
             mean_var_file = torch.from_numpy(np.load(os.path.join(mean_var_dir, date + "_" + str(lt) + ".npy")))
             if self.config.n_var == 3:
@@ -124,8 +125,8 @@ class ISDataset(Dataset):
 
             # Predict the residue of the members (members - ensemble mean) instead of the members
             if self.config.predict_residue:
-                normalized_ensemble_mean_tensor = mean_var_file[0] / mean_var_file[1].expand_as(torch.zeros(self.n_conditioning_sets, self.config.n_var*self.config.n_conditions, self.height_dim, self.width_dim))
-                condition_tensor = torch.sub(condition_tensor, normalized_ensemble_mean_tensor)
+                ensemble_mean_tensor = mean_var_file[0].unsqueeze(0).expand(self.n_conditioning_sets, -1, -1, -1).repeat(1, self.config.n_conditions, 1, 1)
+                condition_tensor = torch.sub(condition_tensor, ensemble_mean_tensor)
 
             # Using the mean and/or the var of the ensemble as additionnal conditions
             if mean_cond:
@@ -134,16 +135,18 @@ class ISDataset(Dataset):
             if var_cond:
                 var = mean_var_file[1].unsqueeze(0).expand(self.n_conditioning_sets, -1, -1, -1)
                 condition_tensor = torch.cat([condition_tensor, var], dim=1)
-        else:
-            normalized_ensemble_mean_tensor = torch.zeros(self.n_conditioning_sets, self.config.n_var*self.config.n_conditions, self.height_dim, self.width_dim) # 0 tensor -> member = member + 0 when sampling
 
 
         sample_id = re.search(r"\d+", file_name).group()
+<<<<<<< HEAD
 <<<<<<< HEAD
         return {"id_in_csv": idx, "img": sample, "img_denorm":sample_denorm, "img_id": sample_id, "condition_tensor": condition_tensor, "condition_tensor_denorm" : condition_tensor_denorm,"member_id": member, "date": date, "leadtime": lt}
 =======
         return {"id_in_csv": idx, "img": sample, "img_id": sample_id, "condition_tensor": condition_tensor, "ensemble mean tensor": normalized_ensemble_mean_tensor, "member_id": member, "date": date, "leadtime": lt}
 >>>>>>> c9f46e6 (Enables training wit the "predict_residue" option)
+=======
+        return {"id_in_csv": idx, "img": sample, "img_id": sample_id, "condition_tensor": condition_tensor, "ensemble mean tensor": ensemble_mean_tensor, "member_id": member, "date": date, "leadtime": lt}
+>>>>>>> e7ab104 (bug fix on the way to retrieve and broadcast the)
 
     def get_conditioning_members(self, ensemble_df, idx,return_denorm=False):
         """
