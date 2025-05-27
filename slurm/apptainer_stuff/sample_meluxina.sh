22b35adaa9cc4fa169df2f3a004e5c6e5720a183
sanchezvvictor <victor.sanchez@meteo.fr>
2025-05-27 13:15:02 +0200

    modif config script

--- slurm/apptainer_stuff/sample_meluxina.sh
+++ slurm/apptainer_stuff/sample_meluxina.sh
@@ -36,4 +36,4 @@ apptainer run --nv /project/home/p200177/DE_371/resources/apptainer_container/fi
     python3 -m torch.distributed.run \
     --standalone \
     --nproc_per_node=4 ./main.py \
-    --yaml_path="/project/home/p200177/DE_371/experiments_WP1/DIFFUSION_experiments_AROME/sdedit/sampling_sdedit_ddpm/ed_sampling_config/config_sample_sdedit_ED_val_225.yml"
+    --yaml_path="path/to/sample/config.yml"
