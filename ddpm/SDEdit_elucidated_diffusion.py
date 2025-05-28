113cfd895af636a04b7378ec1145184d6f53a078
sanchezvvictor <victor.sanchez@meteo.fr>
2025-05-28 15:27:39 +0200

    SDEdit now compatible with Elucidated Diffusion!!

--- ddpm/SDEdit_elucidated_diffusion.py
+++ ddpm/SDEdit_elucidated_diffusion.py
@@ -47,7 +47,7 @@ class SDEditElucidatedDiffusion(ElucidatedDiffusion):
             S_noise=S_noise
         )
         
-        assert num_edition_timesteps < num_sample_steps
+        assert num_edition_timesteps <= num_sample_steps
         self.num_edition_timesteps=num_edition_timesteps
 
     
@@ -76,46 +76,100 @@ class SDEditElucidatedDiffusion(ElucidatedDiffusion):
             0.
         )
 
-        # sigmas = sigmas[:self.num_edition_timesteps]
-        # gammas = gammas[:self.num_edition_timesteps]
-        # Not operationnal ! 
-        sigmas_and_gammas = list(zip(sigmas[:-1], sigmas[1:], gammas[:-1]))
 
-        # Noising condition entirely
-        _sigmas = self.noise_distribution(batch_size)
-        # print('sigmas.shape',_sigmas.shape)
-        padded_sigmas = rearrange(_sigmas, 'b -> b 1 1 1')
-        # print('padded_sigmas.shape',padded_sigmas.shape)
-        noise = torch.randn_like(condition)
+        sigmas_and_gammas = list(
+            zip(
+            sigmas[num_sample_steps - self.num_edition_timesteps:-1],
+            sigmas[num_sample_steps - self.num_edition_timesteps+1:],
+            gammas[num_sample_steps - self.num_edition_timesteps:-1]
+            )
+        )
 
-        images = condition + padded_sigmas * noise  # alphas are 1. in the paper
+        # Noising condition
+        condition = normalize_to_neg_one_to_one(condition)
+        init_sigma = sigmas[num_sample_steps - self.num_edition_timesteps]
+        noise = torch.randn_like(condition, device = self.device)
 
-        # gradually denoise
+        images = init_sigma * noise  + condition * bool(num_sample_steps - self.num_edition_timesteps)
+
+        # Gradually denoising
 
         for sigma, sigma_next, gamma in tqdm(sigmas_and_gammas, desc = 'sampling time step'):
             sigma, sigma_next, gamma = map(lambda t: t.item(), (sigma, sigma_next, gamma))
 
-            eps = self.S_noise * torch.randn(shape, device = self.device) # stochastic sampling
+            eps = self.S_noise * torch.randn(shape, device = self.device) # stochastic sampling # Algorithm 2 : line 4
 
-            sigma_hat = sigma + gamma * sigma
-            images_hat = images + sqrt(sigma_hat ** 2 - sigma ** 2) * eps
+            sigma_hat = sigma + gamma * sigma # Algorithm 2 : line 5
+            images_hat = images + sqrt(sigma_hat ** 2 - sigma ** 2) * eps # Algorithm 2 : line 6
 
-            model_output = self.preconditioned_network_forward(images_hat, sigma_hat, self_cond=None, clamp = clamp)
-            denoised_over_sigma = (images_hat - model_output) / sigma_hat
+            self_cond = condition if self.self_condition else None
+
+            model_output = self.preconditioned_network_forward(images_hat, sigma_hat, self_cond, clamp = clamp)
+            denoised_over_sigma = (images_hat - model_output) / sigma_hat # Algorithm 2 : line 7
 
             images_next = images_hat + (sigma_next - sigma_hat) * denoised_over_sigma
 
             # second order correction, if not the last timestep
 
             if sigma_next != 0:
+                self_cond = condition if self.self_condition else None
 
-                model_output_next = self.preconditioned_network_forward(images_next, sigma_next, self_cond=None, clamp = clamp)
+                model_output_next = self.preconditioned_network_forward(images_next, sigma_next, self_cond, clamp = clamp)
                 denoised_prime_over_sigma = (images_next - model_output_next) / sigma_next
                 images_next = images_hat + 0.5 * (sigma_next - sigma_hat) * (denoised_over_sigma + denoised_prime_over_sigma)
 
             images = images_next
+            x_start = model_output_next if sigma_next != 0 else model_output
 
-        return images
+        #images = images.clamp(-1., 1.)
+        return unnormalize_to_zero_to_one(images)
+    
+    @torch.no_grad()
+    def sample_using_dpmpp(self,
+                batch_size = 16,
+                num_sample_steps = None,
+                condition=None,
+                clamp = False
+        ):
+        """
+        thanks to Katherine Crowson (https://github.com/crowsonkb) for figuring it all out!
+        https://arxiv.org/abs/2211.01095
+        """
+
+        device, num_sample_steps = self.device, default(num_sample_steps, self.num_sample_steps)
+
+        sigmas = self.sample_schedule(num_sample_steps)
+
+        shape = (batch_size, self.channels, self.image_size, self.image_size)
+        # Noising condition
+        condition = normalize_to_neg_one_to_one(condition)
+        init_sigma = sigmas[num_sample_steps - self.num_edition_timesteps]
+        noise = torch.randn_like(condition, device = self.device)
+
+        images = init_sigma * noise  + condition * bool(num_sample_steps - self.num_edition_timesteps)
+
+        sigma_fn = lambda t: t.neg().exp()
+        t_fn = lambda sigma: sigma.log().neg()
+
+        old_denoised = None
+        for i in tqdm(range(len(sigmas) - 1)):
+            denoised = self.preconditioned_network_forward(images, sigmas[i].item())
+            t, t_next = t_fn(sigmas[i]), t_fn(sigmas[i + 1])
+            h = t_next - t
+
+            if not exists(old_denoised) or sigmas[i + 1] == 0:
+                denoised_d = denoised
+            else:
+                h_last = t - t_fn(sigmas[i - 1])
+                r = h_last / h
+                gamma = - 1 / (2 * r)
+                denoised_d = (1 - gamma) * denoised + gamma * old_denoised
+
+            images = (sigma_fn(t_next) / sigma_fn(t)) * images - (-h).expm1() * denoised_d
+            old_denoised = denoised
+
+        images = images.clamp(-1., 1.)
+        return unnormalize_to_zero_to_one(images)
 
 
     # training
