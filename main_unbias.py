501e5332a41b037149ceddb32bbf56101efb1798
sanchezvvictor <victor.sanchez@meteo.fr>
2025-08-20 09:52:58 +0200

    updates

--- main_unbias.py
+++ main_unbias.py
@@ -68,14 +68,15 @@ if __name__=="__main__" :
 
     list_dates = df_extract['Date'].unique()
 
-
     #################### main loop ##################
     for date_ in list_dates:
         datename = date_.strftime('%Y-%m-%d')
         for lt in params.leadtimes:
-            save_path = f'{params.output_dir}4var_fake_ensemble_{datename}_{lt}.npy'
+            # save_path = f'{params.output_dir}4var_fake_ensemble_{datename}_{lt}.npy'
+            save_path = f'{params.output_dir}genFsemble_{datename}_{lt}_1000_16.npy'
             if not os.path.isfile(save_path):
-                print(f'Unbiasing 4var_fake_ensemble_{datename}_{lt}.npy.')
+                # print(f'Unbiasing 4var_fake_ensemble_{datename}_{lt}.npy.')
+                print(f'Unbiasing genFsemble_{datename}_{lt}.npy.')
                 # Loading AROME ensemble   
                 df0 = df_extract[(df_extract['Date']==date_) & (df_extract['LeadTime']==lt-1)]
                 Nb = len(df0)
@@ -83,15 +84,17 @@ if __name__=="__main__" :
                 for i,s in enumerate(df0['Name']):
                     sn = np.load(f'{params.real_data_dir}{s}.npy')[params.var_indices,:,:].astype(np.float32)
                     Ens_AROME[i] = sn
-
+                Ens_AROME = Ens_AROME[:,1:,:,:]
                 # Loading Generated ensemble
-                Ens_Gen = np.load(f'{params.gen_data_dir}4var_fake_ensemble_{datename}_{lt}.npy')
+                # Ens_Gen = np.load(f'{params.gen_data_dir}4var_fake_ensemble_{datename}_{lt}.npy')
+                Ens_Gen = np.load(f'{params.gen_data_dir}genFsemble_{datename}_{lt}_1000_16.npy')
                 
                 # Creating unbiased new ensemble
                 Ens_Gen_unbiased = Ens_Gen + np.expand_dims(Ens_AROME.mean(axis=0),0) - np.expand_dims(Ens_Gen.mean(axis=0),0)
                 np.save(save_path, Ens_Gen_unbiased)
             else :
-                print(f'4var_fake_ensemble_{datename}_{lt}.npy already exists, Switching to next sample.')
+                # print(f'4var_fake_ensemble_{datename}_{lt}.npy already exists, Switching to next sample.')
+                print(f'Unbiasing genFsemble_{datename}_{lt}_1000_16.npy already exists, Switching to next sample.')
 
     print('Unbiasing done.')
 
